3fd4de564bbdb9b6a21ee99f814be492d600e87f
Valentinesmin <aonswitch@hotmail.com>
2024-03-24 13:55:36 +0700

    start

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int count;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+    // How to win
+    void SetCountText()
+    {
+        countText.text = "count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You Win";
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce( movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive( false );
+            count = count + 1;  //count ++; //count += 1;
+            SetCountText();
+        }
+    }
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Update()
+    {
+        transform.Rotate( new Vector3( 15,30,45 ) * Time.deltaTime);
+    }
+}
